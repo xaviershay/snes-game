71c96ee12b25050a004d7717a90409b791848f70
Xavier Shay <xavier@rhnh.net>
2015-02-06 22:12:22 -0800

    Trying to get working.

--- main.asm
+++ main.asm
@@ -11,6 +11,43 @@
 current_color db
 .ENDE
 
+
+.bank 1 slot 0		; We'll use bank 1
+.org 0
+.section "PrettyPictures"
+Colors:
+
+.DB %00000011, %11100000 ; Green
+.DB %00000000, %00011111 ; Red
+.DB %01111100, %00000000 ; Blue
+.ends
+
+.bank 0 slot 0
+.org 0
+.section "Vblank"
+; TODO: Not sure this is even getting called
+VBlank:
+  brk ; Temporary crash
+  lda $4210       ; Clear NMI flag
+;  lda $4212	; get joypad status
+;  and #%00000001	; if joy is not ready
+;  bne VBlank	; wait
+;  lda $4219	; read joypad (BYSTudlr)
+;  beq ++; if zero, we're done
+;
+;  lda current_color
+;  inc A
+;  inc A
+;  cmp #06
+;  bcc +
+;  and #0
+;+ sta current_color
+  ;stz current_color.l
+
+  ;jsl paint_background
+++ rti
+.ENDS
+
 .BANK 0 SLOT 0
 .ORG 0
 .SECTION "MainCode"
@@ -19,11 +56,11 @@ Start:
   Snes_Init           ; Init Snes :)
 
   stz reg_cgadd       ; Edit color 0, which is the screen color
-  stz current_color
+  stz current_color.l
 
   ; Temporary different initial color
-  lda $04
-  sta current_color
+  lda #04
+  sta current_color.l
 
   ; TODO: This should happen on vblank
   jsl paint_background
@@ -31,44 +68,22 @@ Start:
   lda #$0F
   sta reg_inidisp     ; Turn on screen, full brightness
 
+  lda #%10000000	; enable NMI
+  sta $4200
+
 forever:
   wai
   jmp forever
 
-; TODO: Not sure this is even getting called
-VBlank:
-  lda $4212	; get joypad status
-  and #%00000001	; if joy is not ready
-  bne VBlank	; wait
-  lda $4219	; read joypad (BYSTudlr)
-  beq ++; if zero, we're done
-
-  lda current_color
-  inc A
-  inc A
-  cmp #06
-  bcc +
-  and #0
-+ sta current_color
-
-  jsl paint_background
-++ rti
 
 paint_background:
   stz reg_cgadd       ; Edit color 0, which is the screen color
   ldx current_color
   inx
-  lda :colors,x
+  lda Colors.l,x
   sta reg_cgdata
-  dex
-  lda :colors,x
+  lda Colors.l-1,x
   sta reg_cgdata
   rtl
-
-colors:
-
-.DB %00000011, %11100000 ; Green
-.DB %00000000, %00011111 ; Red
-.DB %01111100, %00000000 ; Blue
 
 .ENDS
