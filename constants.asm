1b9e8abd9e021cda55498100ccae37ecb204ab7d
Xavier Shay <xavier@rhnh.net>
2015-02-07 10:49:24 -0800

    color cycle works

--- constants.asm
+++ constants.asm
@@ -7,3 +7,14 @@ reg_inidisp db
 reg_cgadd db
 reg_cgdata db
 .ENDE
+
+.DEFINE reg_interrupt_enable $4200
+.EXPORT reg_interrupt_enable
+
+; This is listed as PPU register. Why do we read this for joypad ready? Why not
+; IO port?
+.DEFINE reg_joy_status $4212
+.EXPORT reg_joy_status
+
+.DEFINE reg_joy1_h $4219
+.EXPORT reg_joy1_h
--- main.asm
+++ main.asm
@@ -9,81 +9,87 @@
 
 .ENUM $0000
 current_color db
+joypad1 db
+joypad1_new db
+joypad1_delta_on db
 .ENDE
-
-
-.bank 1 slot 0		; We'll use bank 1
-.org 0
-.section "PrettyPictures"
-Colors:
-
-.DB %00000011, %11100000 ; Green
-.DB %00000000, %00011111 ; Red
-.DB %01111100, %00000000 ; Blue
-.ends
-
-.bank 0 slot 0
-.org 0
-.section "Vblank"
-; TODO: Not sure this is even getting called
-VBlank:
-  brk ; Temporary crash
-  lda $4210       ; Clear NMI flag
-;  lda $4212	; get joypad status
-;  and #%00000001	; if joy is not ready
-;  bne VBlank	; wait
-;  lda $4219	; read joypad (BYSTudlr)
-;  beq ++; if zero, we're done
-;
-;  lda current_color
-;  inc A
-;  inc A
-;  cmp #06
-;  bcc +
-;  and #0
-;+ sta current_color
-  ;stz current_color.l
-
-  ;jsl paint_background
-++ rti
-.ENDS
 
 .BANK 0 SLOT 0
 .ORG 0
 .SECTION "MainCode"
 
 Start:
-  Snes_Init           ; Init Snes :)
+  Snes_Init
 
-  stz reg_cgadd       ; Edit color 0, which is the screen color
+  ; Zero out memory
   stz current_color.l
+  stz joypad1.l
 
-  ; Temporary different initial color
-  lda #04
-  sta current_color.l
-
-  ; TODO: This should happen on vblank
-  jsl paint_background
+  jsl paint_background ; Initial paint
 
   lda #$0F
-  sta reg_inidisp     ; Turn on screen, full brightness
+  sta reg_inidisp ; Turn on screen, full brightness
 
-  lda #%10000000	; enable NMI
-  sta $4200
+  lda #%10000001	; enable NMI and joypad
+  sta reg_interrupt_enable
 
 forever:
   wai
   jmp forever
 
+; VBlank is called every frame (NMI interrupt)
+VBlank:
+  ; ===== INPUT PROCESSING
+  ; Loop until joypad registers are ready to be read
+  lda reg_joy_status
+  and #%00000001
+  bne VBlank
+
+  lda reg_joy1_h ; read BYSTudlr bits from joypad
+
+  ; Figure out any buttons that have just been depressed, store in delta_on
+  sta joypad1_new
+  eor joypad1
+  and joypad1_new
+  sta joypad1_delta_on
+  lda joypad1_new
+  sta joypad1
+
+  ; ===== LOGIC
+  ; If B has not been newly pressed, skip to end.
+  lda joypad1_delta_on
+  and #%10000000
+  beq ++
+
+  ; Increment color twice because each color is 2 bytes long.
+  lda current_color
+  inc A
+  inc A
+
+  ; If color is greater than 6 (3 colors) reset to 0
+  cmp #06
+  bcc +
+  and #0
+
++ sta current_color
+
+  jsl paint_background
+
+++ rti
 
 paint_background:
   stz reg_cgadd       ; Edit color 0, which is the screen color
   ldx current_color
-  inx
+  lda Colors.l+1,x
+  sta reg_cgdata
   lda Colors.l,x
   sta reg_cgdata
-  lda Colors.l-1,x
-  sta reg_cgdata
   rtl
+
+Colors:
+
+.DB %00000011, %11100000 ; Green
+.DB %00000000, %00011111 ; Red
+.DB %01111100, %00000000 ; Blue
 
 .ENDS
